00462f24d769f1dffe554cb924217834c786538e
Anthony Bui <anthonyb50@gmail.com>
2023-01-03 19:46:46 -0500

    Complete exporting request handling function

--- server/request-handler.js
+++ server/request-handler.js
@@ -8,7 +8,9 @@ in basic-server.js, but it won't work as is.
 You'll have to figure out a way to export this function from
 this file and include it in basic-server.js so that it actually works.
 
+
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
+
 
 **************************************************************/
 
@@ -31,56 +33,10 @@ var defaultCorsHeaders = {
 
 var requestHandler = function(request, response) {
 
+  const { method, url } = request;
+
   var headers = defaultCorsHeaders;
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
-};
-
-
-
-module.exports.requestHandler = requestHandler;
-
-
-http.createServer((request, response) => { //<--- This is requestHandler, to be erased
-  const { headers, method, url } = request; //var headers = defaultCorsHeaders;
   let body = []; // This is where the data is stored
   request.on('error', (err) => { // If there is an error in request, will console log error
     console.error(err);
@@ -94,21 +50,100 @@ http.createServer((request, response) => { //<--- This is requestHandler, to be 
       console.error(err);
     });
 
-    headers['Content-Type'] = 'text/plain'; //
+
+    // Request and Response come from node's http module.
+    //
+    // They include information about both the incoming request, such as
+    // headers and URL, and about the outgoing response, such as its status
+    // and content.
+    //
+    // Documentation for both request and response can be found in the HTTP section at
+    // http://nodejs.org/documentation/api/
+
+    // Do some basic logging.
+    //
+    // Adding more logging to your server can be an easy way to get passive
+    // debugging help, but you should always be careful about leaving stray
+    // console.logs in your code.
+    console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
+    // The outgoing status.
+    var statusCode = 200;
+
+    // See the note below about CORS headers.
+
+
+    // Tell the client we are sending them plain text.
+    //
+    // You will need to change this if you are sending something
+    // other than plain text, like JSON or HTML.
+
+    headers['Content-Type'] = 'text/plain';
+    //   headers['Content-Type'] = 'application/json';
+
+    // .writeHead() writes to the request line and headers of the response,
+    // which includes the status and all headers.
+    body = Buffer.concat(body).toString(); //Body is now reassigned using storage and ?Buffer?
+
+    response.on('error', (err) => { //Error on response, console log error
+      console.error(err);
+    });
     response.writeHead(statusCode, headers);
 
-    /*  response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    Note: the 2 lines above could be replaced with this next one:
-    response.writeHead(200, {'Content-Type': 'application/json'}) */
-
     const responseBody = { headers, method, url, body };
-
     response.write(JSON.stringify(responseBody));
-    response.end();
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.end(JSON.stringify(responseBody))
-
-    // END OF NEW STUFF
+    // Make sure to always call response.end() - Node may not send
+    // anything back to the client until you do. The string you pass to
+    // response.end() will be the body of the response - i.e. what shows
+    // up in the browser.
+    //
+    // Calling .end "flushes" the response's internal buffer, forcing
+    // node to actually send all the data over to the client.
+    response.end('Hello, World!');
   });
-}).listen(8080);
+
+};
+
+
+
+exports.requestHandler = requestHandler;
+
+
+//http.createServer((request, response) => { //<--- This is requestHandler, to be erased
+//const { headers, method, url } = request; //var headers = defaultCorsHeaders; // {headers: defaultCorsHeaders, method: request.method, url: request.url }
+//headers = request headers
+//var headers = defaultCors
+//var method = request.method
+//var url = request.url
+// let body = []; // This is where the data is stored
+// request.on('error', (err) => { // If there is an error in request, will console log error
+//   console.error(err);
+// }).on('data', (chunk) => { //Otherwise, once data is received, push into storage
+//   body.push(chunk);
+// }).on('end', () => { //Response goes in here, at the end of receiving data request
+
+// body = Buffer.concat(body).toString(); //Body is now reassigned using storage and ?Buffer?
+
+//  response.on('error', (err) => { //Error on response, console log error
+//   console.error(err);
+// });
+
+// headers['Content-Type'] = 'text/plain';
+
+//response.writeHead(statusCode, headers);
+
+//response.statusCode = 200;
+// response.setHeader('Content-Type', 'application/json');
+// Note: the 2 lines above could be replaced with this next one:
+// response.writeHead(200, {'Content-Type': 'application/json'})
+
+// const responseBody = { headers, method, url, body };
+
+// response.write(JSON.stringify(responseBody));
+//response.end();
+// Note: the 2 lines above could be replaced with this next one:
+// response.end(JSON.stringify(responseBody))
+
+// END OF NEW STUFF
+//   });
+// }).listen(8080);
